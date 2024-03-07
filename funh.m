8936b105980af717ef0cb589a8498e55b7d85449
mjmoreta <152295348+mjmoreta@users.noreply.github.com>
2024-03-07 18:51:44 +0100

    Add files via upload

---
+++ funh.m
@@ -0,0 +1,6 @@
+function valz=funh(tt,xx)
+
+% This function is used to calculate h(t,x)
+
+valz=-sin(xx+tt)+cos(xx+tt)-cos(xx+tt)^2;
+
---
+++ uut.m
@@ -0,0 +1,4 @@
+function valz=uut(tt,xx)
+
+% This function is used to calculate the boundary values of u_t
+valz=-sin(xx+tt);
